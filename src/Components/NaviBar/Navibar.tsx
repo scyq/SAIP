6550611d3138f268dcd296cff7c5d091912e0892
Yuqing_Chen <chenyuqing0429@foxmail.com>
2021-04-04 22:30:28 +0800

    Topbar

---
+++ src/Components/NaviBar/Navibar.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepConnector from '@material-ui/core/StepConnector';
+import SettingsIcon from '@material-ui/icons/Settings';
+import clsx from 'clsx';
+import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects';
+import ColorLensIcon from '@material-ui/icons/ColorLens';
+import InvertColorsIcon from '@material-ui/icons/InvertColors';
+import './Navibar.css';
+
+/* Icon的样式 */
+const useColorlibStepIconStyles = makeStyles({
+    root: {
+        backgroundColor: '#ccc',
+        zIndex: 1,
+        color: '#fff',
+        width: 50,
+        height: 50,
+        display: 'flex',
+        borderRadius: '50%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    active: {
+        backgroundImage:
+            'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
+        boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)',
+    },
+    completed: {
+        backgroundImage:
+            'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
+    },
+});
+
+
+const ColorlibConnector = withStyles({
+    alternativeLabel: {
+        top: 22,
+    },
+    active: {
+        '& $line': {
+            backgroundImage:
+                'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
+        },
+    },
+    completed: {
+        '& $line': {
+            backgroundImage:
+                'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
+        },
+    },
+    line: {
+        height: 3,
+        border: 0,
+        backgroundColor: '#eaeaf0',
+        borderRadius: 1,
+    },
+})(StepConnector);
+
+/* Stepper 每一步的Icon 在这里修改 */
+function ColorlibStepIcon(props: any) {
+    const classes = useColorlibStepIconStyles();
+    const { active, completed } = props;
+
+    const icons = {
+        1: <SettingsIcon />,
+        2: <ColorLensIcon />,
+        3: <InvertColorsIcon />,
+        4: <EmojiObjectsIcon />
+    };
+
+    return (
+        <div
+            className={clsx(classes.root, {
+                [classes.active]: active,
+                [classes.completed]: completed,
+            })}
+        >
+            {icons[String(props.icon)]}
+        </div>
+    );
+}
+
+export default function Navibar(props: any) {
+    return (
+        <div className="root" >
+            <Stepper alternativeLabel activeStep={props.activeStep} connector={<ColorlibConnector></ColorlibConnector>}>
+                {steps.map((label) => (
+                    <Step key={label}>
+                        <StepLabel StepIconComponent={ColorlibStepIcon}>
+                            {label}
+                        </StepLabel>
+                    </Step>
+                ))}
+            </Stepper>
+        </div>
+    )
+}
---
+++ src/Components/Topbar/Topbar.tsx
@@ -0,0 +1,15 @@
+import { AppBar, Button, Toolbar, Typography } from '@material-ui/core';
+import React from 'react';
+import './Topbar.css';
+
+export default function Topbar() {
+    return (
+        <AppBar position="static" className="root">
+            <Toolbar>
+                <Typography variant="h6" className="title">
+                    A Prototype Tool for Semi-Automatic User Interface Prototyping
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    );
+}
