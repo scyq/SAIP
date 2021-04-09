2a2c67785cd92607cc9b3199ddf0d068085b6d84
Yuqing_Chen <chenyuqing0429@foxmail.com>
2021-04-09 15:54:48 +0800

    测试训练失败

--- algorithm/classification.py
+++ algorithm/classification.py
@@ -1,1 +1,10 @@
 import fasttext
+
+
+def train():
+    model = fasttext.train_supervised(input="train.train", minCount=1)
+    model.save_model("classification.bin")
+
+
+if __name__ == "__main__":
+    train()
