a13ecb807b20ff3e9ecb4feaae74321cc9431ad9
Yuqing_Chen <chenyuqing0429@foxmail.com>
2021-04-09 16:42:29 +0800

    Fixed URL Bugs

--- algorithm/classification.py
+++ algorithm/classification.py
@@ -2,9 +2,17 @@ import fasttext
 
 
 def train():
-    model = fasttext.train_supervised(input="train.train", minCount=1)
-    model.save_model("classification.bin")
+    model = fasttext.train_supervised(
+        input="./algorithm/train.txt", minCount=1)
+    model.save_model("./algorithm/classification.bin")
+
+
+def test():
+    model = fasttext.load_model("./algorithm/classification.bin")
+    output = model.predict("I wanna a youtube like website")
+    print(output)
 
 
 if __name__ == "__main__":
     train()
+    test()
